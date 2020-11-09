0de7d88d3c534c8c197e1c9b91a8d74c70fae5cb
egor_antrop <mougraaa@gmail.com>
2020-11-09 20:27:37 +0400

    create hello world app

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+     /*   System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello world!");
+
+     */
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
